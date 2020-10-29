7c6963e27e303eb6ad5e708d8c5e2d100a2bc566
TuanGiang <tuangiang1092@gmail.com>
2020-10-29 17:07:28 +0700

    Update Compression.java

--- android/src/main/java/com/yalantis/ucrop/util/Compression.java
+++ android/src/main/java/com/yalantis/ucrop/util/Compression.java
@@ -29,50 +29,54 @@ public class Compression {
     public  static File resize(String originalImagePath, int maxWidth, int maxHeight, int quality) throws IOException {
         Bitmap original = BitmapFactory.decodeFile(originalImagePath);
 
-        int width = original.getWidth();
-        int height = original.getHeight();
+        if(original != null) {
+            int width = original.getWidth();
+            int height = original.getHeight();
 
-        // Use original image exif orientation data to preserve image orientation for the resized bitmap
-        ExifInterface originalExif = new ExifInterface(originalImagePath);
-        int originalOrientation = originalExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+            // Use original image exif orientation data to preserve image orientation for the resized bitmap
+            ExifInterface originalExif = new ExifInterface(originalImagePath);
+            int originalOrientation = originalExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
 
-        Matrix rotationMatrix = new Matrix();
-        int rotationAngleInDegrees = getRotationInDegreesForOrientationTag(originalOrientation);
-        rotationMatrix.postRotate(rotationAngleInDegrees);
+            Matrix rotationMatrix = new Matrix();
+            int rotationAngleInDegrees = getRotationInDegreesForOrientationTag(originalOrientation);
+            rotationMatrix.postRotate(rotationAngleInDegrees);
 
-        float ratioBitmap = (float) width / (float) height;
-        float ratioMax = (float) maxWidth / (float) maxHeight;
+            float ratioBitmap = (float) width / (float) height;
+            float ratioMax = (float) maxWidth / (float) maxHeight;
 
-        int finalWidth = maxWidth;
-        int finalHeight = maxHeight;
+            int finalWidth = maxWidth;
+            int finalHeight = maxHeight;
 
-        if (ratioMax > ratioBitmap) {
-            finalWidth = (int) ((float) maxHeight * ratioBitmap);
-        } else {
-            finalHeight = (int) ((float) maxWidth / ratioBitmap);
+            if (ratioMax > ratioBitmap) {
+                finalWidth = (int) ((float) maxHeight * ratioBitmap);
+            } else {
+                finalHeight = (int) ((float) maxWidth / ratioBitmap);
+            }
+
+            Bitmap resized = Bitmap.createScaledBitmap(original, finalWidth, finalHeight, true);
+            resized = Bitmap.createBitmap(resized, 0, 0, finalWidth, finalHeight, rotationMatrix, true);
+
+            File imageDirectory = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES);
+
+            if (!imageDirectory.exists()) {
+                Log.d("image-crop-picker", "Pictures Directory is not existing. Will create this directory.");
+                imageDirectory.mkdirs();
+            }
+
+            File resizeImageFile = new File(imageDirectory, UUID.randomUUID() + ".jpg");
+
+            OutputStream os = new BufferedOutputStream(new FileOutputStream(resizeImageFile));
+            resized.compress(Bitmap.CompressFormat.JPEG, quality, os);
+
+            os.close();
+            original.recycle();
+            resized.recycle();
+
+            return resizeImageFile;
+        }else{
+            return new File(originalImagePath);
         }
-
-        Bitmap resized = Bitmap.createScaledBitmap(original, finalWidth, finalHeight, true);
-        resized = Bitmap.createBitmap(resized, 0, 0, finalWidth, finalHeight, rotationMatrix, true);
-        
-        File imageDirectory = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-
-        if(!imageDirectory.exists()) {
-            Log.d("image-crop-picker", "Pictures Directory is not existing. Will create this directory.");
-            imageDirectory.mkdirs();
-        }
-
-        File resizeImageFile = new File(imageDirectory, UUID.randomUUID() + ".jpg");
-
-        OutputStream os = new BufferedOutputStream(new FileOutputStream(resizeImageFile));
-        resized.compress(Bitmap.CompressFormat.JPEG, quality, os);
-
-        os.close();
-        original.recycle();
-        resized.recycle();
-
-        return resizeImageFile;
     }
 
 
